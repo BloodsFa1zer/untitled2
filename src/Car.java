a238c0c66ee07274b562f6daf19f589e5e80c293
BloodsFa1zer <62332655+BloodsFa1zer@users.noreply.github.com>
2020-03-30 12:15:26 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car implements Cloneable {
+    int amountOfDoor;
+    int amountOfWheel;
+    String marka;
+    String color;
+    String model;
+
+
+
+    public Car(int amountOfWheel, int amountOfDoor, String model, String marka, String color){
+        this.amountOfDoor = amountOfDoor;
+        this.amountOfWheel = amountOfWheel;
+        this.color = color;
+        this.model = model;
+        this.marka = marka;
+    }
+
+    protected Object clone() throws  CloneNotSupportedException{
+        return super.clone();
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        Car c = new Car(4, 3, "SINAbooba", "G100000505", "red");
+        Car clone1 = (Car) c.clone();
+        Engine e = new Engine(29, "hukihih", "Eojijoi221213", 300, 60);
+        Engine clone2 = (Engine) e.clone2();
+
+
+        System.out.println(clone1);
+
+        System.out.println(clone2);
+
+        clone1.model = new String("GIBABOBA");
+        clone2.markaMashini = new String("oijojo");
+
+
+        System.out.println(clone1);
+
+        System.out.println(clone2);
+    }
+}
